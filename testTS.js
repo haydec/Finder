6b4c4120cd60df7cd90726bcb33a422812c91fc5
haydecUbuntuWork <cillianhayde@protonmail.com>
2025-02-24 17:22:26 +0900

    TypeScript

---
+++ testTS.js
@@ -0,0 +1,5 @@
+"use strict";
+//let genericArray: [] = [];
+//genericArray.push(1);
+let numberArray = [];
+numberArray.push(1);
---
+++ testTS.ts
@@ -0,0 +1,5 @@
+//let genericArray: [] = [];
+//genericArray.push(1);
+
+let numberArray: number[] = [];
+numberArray.push(1);
